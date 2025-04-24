400b8f7765ead299abdfabdae017b09806b285ec
chriswang06 <chrismcwang1@gmail.com>
2025-04-24 19:11:49 -0400

    latest

--- cmake-build-debug/out.asm
+++ cmake-build-debug/out.asm
@@ -1,5 +1,26 @@
 global _start
 _start:
-    mov rax, 60,
-    mov rdi, 100
+    mov rax, 7
+    push rax
+    mov rax, 1
+    push rax
+    mov rax, 2
+    push rax
+    mov rax, 3
+    push rax
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+    push QWORD [rsp + 0]
+
+    mov rax, 60
+    pop rdi
+    syscall
+    mov rax, 60
+    mov rdi, 0
     syscall
--- test.hy
+++ test.hy
@@ -1,1 +1,3 @@
-exit 100;
+let x = 7;
+let y = 1+2+3;
+exit(y);
